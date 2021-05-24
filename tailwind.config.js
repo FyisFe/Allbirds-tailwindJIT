1f1998bb97f9a7a8fc7c3c92a79adc1103c47070
Yi Feng <yfeng28-c@my.cityu.edu.hk>
2021-05-25 01:22:52 +0800

    done mobile breakpoint

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    mode: 'jit',
+    purge: ['./*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'system-ui'],
+            },
+            colors: {
+                lime: colors.lime,
+                green: colors.green,
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
+}
